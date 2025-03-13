d5ec371534b6666d7f191a57a255d72b672faedc
vyshnavmt <vyshnav.mt@razorthink.com>
2025-03-13 11:26:49 +0530

    vyshnav changes

--- model_test/run_falcon.sh
+++ model_test/run_falcon.sh
@@ -1,11 +1,8 @@
-export HUGGING_FACE_HUB_TOKEN="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"
-
-
 nohup vllm serve "/data/falcon3-7b-instruct" \
      --tokenizer-mode auto \
      --load-format auto \
      --config-format auto \
-     --max-model-len 3000 \
+     --max-model-len 4000 \
      --gpu_memory_utilization 0.90 \
      --cpu-offload-gb 1.5 \
      --seed 369 \
